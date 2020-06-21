cb3eba9d8882dffa0762d7a3e2d58f4bd8f03b0d
moonburnt <moonburnt@disroot.org>
2020-06-21 05:23:26 +0300

    Add files via upload

--- goginst.sh
+++ goginst.sh
@@ -16,9 +16,12 @@ scriptname=`basename "$0"`
 configpath="$HOME/.config/goginst"
 configname="config"
 
-##Optional dependencies
+delfunc="gio trash" #Command used to clean up temporary files
+
+##Optional dependencies and miscelation options that dont need to be altered/configured manually by user. Will be overwritten by values from configfile, kept there in case they dont exist in it
 inno=true
 gogext=true
+interactive=true
 
 ##Functions:
 #If directory doesnt exist - shuts whole script down
@@ -34,6 +37,23 @@ depcheck() {
     if [ ! $(command -v "$1") ]; then
         printf "$1 cant seem to be found on your system.\n"
         false
+    fi
+}
+
+#checks if $1 equal "true" or "false". If yes - prints $2 as message and $1 as variable's state, else - shuts script down with error
+optcheck() {
+    if [ -z "$2" ]; then
+        echo "You're doing it wrong!"
+        exit 1
+    fi
+
+    if [ "$1" == true ]; then
+        printf " - $2: enabled\n"
+    elif [ "$1" == false ]; then
+        printf " - $2: disabled\n"
+    else
+        printf "Detected invalid configuration! Please edit or delete re-generate your config file and try again\n"
+        exit 1
     fi
 }
 
@@ -64,16 +84,34 @@ configmaker() {
     else
         echo "Creating $2"
         #there goes configfile creation. Every new user-configurable variable should be added to this list
-        printf '#This is a settings file for goginst.sh. Change values of variables below according to your setup. General bash rules apply
-downloads="." #Path to directory with gogrepo-downloaded games
-gamedir="./goginst/Games" #Path to directory that will contain your installed games
-tempdir="./goginst/temp" #Path to directory with temporary files
-extractor="./gogextract.py" #Path to gogextract.py, which is used to speed up unpacking on native linux games
-delfunc="gio trash" #Command used to clean up temporary files
+        if [ "$interactive" = true ]; then
+            printf "Enter the path to your directory with gogrepo-downloaded games:\n"
+            read downloads
+            printf "Enter the path to directory that you would like to install games into:\n"
+            read gamedir
+            printf "Enter the path to directory that will be used to store temporary files:\n"
+            read tempdir
+            printf "Enter the path to gogextract.py, in case you have it and want to speed up unpacking of native linux games:\n"
+            read extractor
+
+            echo "Writing values into file"
+        else
+            downloads="$HOME/Downloads/gogrepo/"
+            gamedir="$HOME/.local/share/games/gog/"
+            tempdir="$HOME/.cache/goginst/temp/"
+            extractor="$HOME/gogextract.py"
+        fi
+
+        printf "#This is a settings file for goginst.sh. Change values of variables below according to your setup. General bash rules apply
+downloads=\"$downloads\" #Path to directory with gogrepo-downloaded games
+gamedir=\"$gamedir\" #Path to directory that will contain your installed games
+tempdir=\"$tempdir\" #Path to directory with temporary files
+extractor=\"$extractor\" #Path to gogextract.py, which is used to speed up unpacking on native linux games
 
 ##Optional dependencies
 inno=true #Determines if you want to unpack windows games. Default = true, if innoextract isnt found on your system - will be switched to false
-gogext=true #Determines if you want to use gogextract.py, or rely on built-in version. Default = true, if gogextract.py isnt found on path provided via $extractor - will be switched to false' > "$1/$2"
+gogext=true #Determines if you want to use gogextract.py, or rely on built-in version. Default = true, if gogextract.py isnt found on path provided via $extractor - will be switched to false
+interactive=true #Determines if you want to enable some options that require user to confirm certain actions. Default = true, may be usefull to switch to false if you want goginst to perform as part of automated cronjob or something like that" > "$1/$2"
     fi
 }
 
@@ -188,6 +226,7 @@ if [ ! -f "$extractor" ]; then
 fi
 
 #There are thousand of possible cleanup functions, so there is no way to check if the particular one exists. Lets just assume that user isnt retarded and move to dependency check
+
 #If depcheck has returned false (which happens only in case dependency doesnt exist on your system) - shut script down
 depcheck "unzip" || exit 1
 depcheck "grep" || exit 1
@@ -201,17 +240,16 @@ printf "Running $scriptname with following settings:
  - Temporary files directory: $tempdir
  - Cleanup function: $delfunc \n"
 
-if [ "$gogext" = false ]; then
+if [ "$gogext" == true ]; then
+    printf " - Shell extractor: $extractor\n"
+elif [ "$gogext" == false ]; then
     printf " - Shell extractor: built-in\n"
 else
-    printf " - Shell extractor: $extractor\n"
+    exit 1
 fi
 
-if [ "$inno" = false ]; then
-    printf " - Innoextract support: disabled\n"
-else
-    printf " - Innoextract support: enabled\n"
-fi
+optcheck "$inno" "Innoextract support"
+optcheck "$interactive" "Interactive mode"
 
 #Entering $downloads directory
 cd "$downloads"
@@ -222,29 +260,40 @@ if [ -z "$1" ]; then
     exit 1
 fi
 
-#Trying to guess slug based on input. If there are no matching dirs - shutdown with error, if there is a match but its not dir - shutdown with error, if there are multiple results - (for now) shutdown with error.
+#Trying to guess slug based on input. If there are no matching dirs - shutdown with error, if there is a match but its not dir - shutdown with error, if there are multiple results - depending on state of interactive mode, either shutdown or ask to specify one
 shopt -s nullglob
 search=(*"$1"*)
 shopt -u nullglob
+
+for x in "${!search[@]}"; do
+    if [[ ! -d "${search[x]}" ]]; then
+        unset 'search[x]'
+    fi
+    done
 
 if [ "${#search[@]}" == 0 ]; then
     echo "Couldnt find any games matching your input, abort"
     exit 1
 elif [ "${#search[@]}" == 1 ]; then
-    #Checking if directory named $slug exists in $downloads, which usually means that gogrepo has already downloaded such game
-    if [ -d "${search[@]}" ]; then
-        slug="${search[@]}"
-        echo "Found game with slug $slug, proceed"
-    else
-        echo "${search[@]} exists but isnt directory, abort"
-        exit 1
-    fi
+    dircheck "${search[@]}"
+    slug="${search[@]}"
 else
-    #there should be optional ask to pick some choice, once I will finally decide to make toggle-able interactive mode
-    echo "Found multiple games matching your input. Please try again with something more accurate."
-    echo "Current search returned: ${search[@]}"
-    exit 1
+    if [ "$interactive" == false ]; then
+        echo "Got multiple results. Please try again with something more accurate."
+        echo "Current search returned: ${search[@]}"
+        exit 1
+    else
+        echo "Got multiple results. Please select the correct one:"
+        select choice in "${search[@]}"; do
+        if [[ "$choice" ]]; then
+            echo "You've selected $choice"
+            break
+        fi
+        done
+        slug="$choice"
+    fi
 fi
+echo "Found game with slug $slug, proceed"
 
 #Checking if our $slug directory contain native linux (.sh) or wine's .exe installation files and, if yes - proceed accordingly
 shopt -s nullglob #this way we are avoiding inclusion of search command itself into array - so if there are no valid files, it wont trigger false-positive results
@@ -267,6 +316,9 @@ elif [ "$inno" = true ]; then
     if ! [ "${#exes[@]}" == 0 ]; then
         echo "Found exe installers, proceed"
         install "exe"
+    else
+        echo "No valid files has been found, abort"
+        exit 1
     fi
 else
     echo "No valid files has been found, abort"
